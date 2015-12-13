5dcf8f6107c005dd29c42fd728b5efd3507be7e8
lefteris <llaliotis@gmail.com>
2015-12-13 16:40:19 -0500

    Fixed bug Changed the def was_published_recently and ran the test again. Reported by: Lefteris Laliotis Signed off by: Lefteris Laliotis <llaliotis@gmail.com>

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,9 +1,9 @@
-import datetime
-
 from __future__ import unicode_literals
+import datetime
+from django.utils.encoding import python_2_unicode_compatible
+
+
 from django.utils import timezone
-from django.utils.encoding import 
-python_2_unicode_compatible
 
 from django.db import models
 
@@ -14,9 +14,9 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
+def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
